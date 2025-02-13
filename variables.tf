ec13de574ae57d2addf96916e439c320cb3565b2
Yanislgble <162603211+Yanislgble@users.noreply.github.com>
2025-02-13 15:44:18 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "project" {
+  description = "ID-du-project-gcp"
+  default     = "cloud-devops-yanisarthur-ops"
+}
+
+variable "region" {
+  description = "Région GCP"
+  type        = string
+  default     = "europe-west9"
+}
